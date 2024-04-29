823f7f039c3751777e447734b584e6b39f4ac05a
Alure Akash Harishchandra <83151892+Akashalure37@users.noreply.github.com>
2024-04-29 10:06:54 +0530

    Update janitorSchool.java

--- Practice/src/janitorSchool.java
+++ Practice/src/janitorSchool.java
@@ -1,3 +1,9 @@
+/* The janitor of a high school is extremely efficient. By the end of each day, all of the school's waste is in plastic bags weighing between 1.01 pounds and 3.00 pounds. All plastic bags are then taken to the trash bins outside. One trip is described as selecting a number of bags which together do not weigh more than 3.00 pounds, dumping them in the outside trash can and returning to the school. Given the number of plastic bags n, and the weights of each bag, determine the minimum number of
+trips the janitor has to make.
+Example
+n=5
+weight = [1.01, 1.99, 2.5, 1.5, 1.01]
+*/	
 import java.util.*;
 
 public class janitorSchool {
